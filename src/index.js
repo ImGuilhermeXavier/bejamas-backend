7b6f0d767881e75e24cc1d972529f00d5c540cf2
Guilherme Caçador <gxavier@esapiens.com.br>
2021-11-25 16:53:22 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const app = require('express')();
+const cors = require('cors');
+const { Collection, Get, Documents, Paginate, Lambda } = require('faunadb');
+const faunadb = require('faunadb');
+const q = faunadb.query;
+const client = new faunadb.Client({
+  secret: 'fnAEYuSj4uAAQNKtiy4_0iz5JSYnHy1XiaQaGH--',
+  domain: 'db.us.fauna.com',
+});
+
+app.use(cors());
+app.get('/products', async (req, res) => {
+  try {
+    const doc = await client.query(
+      q.Map(
+        Paginate(Documents(Collection('products'))),
+        Lambda((x) => Get(x))
+      )
+    );
+    res.send(doc);
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+app.listen(process.env.PORT || 5000, () =>
+  console.log('API on http://localhost:5000')
+);
